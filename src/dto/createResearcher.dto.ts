d5ab551f911859082d2bd2e995f79be99def721c
DVastMasters <contatorafaeldutra@outlook.com>
2023-06-21 11:49:35 -0300

    DTO for create researcher user

---
+++ src/dto/createResearcher.dto.ts
@@ -0,0 +1,39 @@
+import { object, string, date } from "zod";
+
+export const createResearcherDTO = object({
+    body: object({
+        personal_data: object({
+            full_name: string({
+                required_error: "Full name is required",
+            }).trim(),
+            phone: string({
+                required_error: "Phone number is required",
+            }).trim(),
+            profile_photo: string(),
+            birth_date: date({
+                required_error: "Birth date is required",
+                invalid_type_error: "That's not a date!",
+            }),
+            countryState: string({
+                required_error: "Country state is required",
+            }).trim(),
+        }),
+        email: string({
+            required_error: "Email is required",
+        })
+            .email("Email is invalid!")
+            .toLowerCase()
+            .trim(),
+        password: string({
+            required_error: "Password is required",
+        }).min(8, "Password too short - should be 8 chars minimium"),
+        password_confirmation: string({
+            required_error: "Password confirmation is required",
+        }),
+        instituition: string({
+            required_error: "Instituition is required",
+        }).trim(),
+    }).refine((data) => data.password === data.password_confirmation, {
+        message: "Passwords do not match",
+    }),
+});
