5992bbac3ddc0295edadf1cfdb963cb66f2ceeb4
DVastMasters <contatorafaeldutra@outlook.com>
2023-06-21 11:50:09 -0300

    interface for researcher mongoose schema

---
+++ src/interface/researcher.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+
+export default interface IResearcher {
+    _id?: Types.ObjectId;
+    personal_data: {
+        full_name: string;
+        phone: string;
+        profile_photo?: string;
+        birth_date: Date;
+        country_state: string;
+    };
+    email: string;
+    password_hash: string;
+    role: string;
+    instituition: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
