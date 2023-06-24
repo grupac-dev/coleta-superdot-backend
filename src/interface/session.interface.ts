464da5df617cf994574a457d34f58aab9314d2d3
DVastMasters <contatorafaeldutra@outlook.com>
2023-06-24 15:24:55 -0300

    creating the session service

--- src/interface/session.interface.ts
+++ src/interface/session.interface.ts
@@ -1,9 +1,11 @@
+import { Types } from "mongoose";
 import Researcher from "./researcher.interface";
 
 export default interface ISession {
+    _id?: Types.ObjectId;
     researcher_id: Researcher["_id"];
     valid: boolean;
     userAgent?: string;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
