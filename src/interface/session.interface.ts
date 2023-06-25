0dab7dfa5dc278847aa47ba0fcab4b5d1eec9b89
DVastMasters <contatorafaeldutra@outlook.com>
2023-06-25 19:33:36 -0300

    refactor: fixing functions names and schemas types

--- src/interface/session.interface.ts
+++ src/interface/session.interface.ts
@@ -2,7 +2,7 @@ import { Types } from "mongoose";
 import Researcher from "./researcher.interface";
 
 export default interface ISession {
-    _id?: Types.ObjectId;
+    _id?: string;
     researcher_id: Researcher["_id"];
     valid: boolean;
     userAgent?: string;
--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,13 +1,10 @@
 import bcrypt from "bcrypt";
 import env from "./validateEnv";
 
-export function hashPassword(rawPassword: string) {
-    return bcrypt.hashSync(rawPassword, env.SALT_WORK_FACTOR);
+export function hashContent(rawContent: string) {
+    return bcrypt.hashSync(rawContent, env.SALT_WORK_FACTOR);
 }
 
-export async function comparePassword(
-    rawPassword: string,
-    hashedPassword: string
-) {
-    return await bcrypt.compare(rawPassword, hashedPassword);
+export async function compareHashes(rawText: string, hashedContent: string) {
+    return await bcrypt.compare(rawText, hashedContent);
 }
