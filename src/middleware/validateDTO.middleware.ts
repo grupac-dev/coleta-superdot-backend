2887d90aea8168137eae9ed3732d8b938e088b75
japaakihiro <larahbrenda@hotmail.com>
2023-06-26 10:18:30 -0300

    refactor: validateDTO export mode changed

--- src/middleware/validateDTO.middleware.ts
+++ src/middleware/validateDTO.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
 
-const validate =
+export const validateDTO =
     (schema: AnyZodObject) =>
     (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -15,5 +15,3 @@ const validate =
             res.status(400).send(e.errors);
         }
     };
-
-export default validate;
