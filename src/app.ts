b14079bab87900159c22a201938e43dadef4b7d9
japaakihiro <larahbrenda@hotmail.com>
2023-06-26 10:24:07 -0300

    enhancement: express app setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { deserializeSession } from "./middleware/deserializeSession.middleware";
+import createHttpError, { isHttpError } from "http-errors";
+import { researcherRouter } from "./route/reseacher.route";
+import { sessionRouter } from "./route/session.route";
 
 const app = express();
 
-app.listen("3000", () => {
-  console.log("Started");
+app.use(express.json());
+
+app.use(deserializeSession);
+
+app.use("/api/researcher", researcherRouter);
+
+app.use("/api/session", sessionRouter);
+
+app.use((req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"));
 });
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "unknown error";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        errorMessage = error.message;
+        statusCode = error.status;
+    }
+
+    res.status(statusCode).json({ error: errorMessage });
+});
+
+export default app;
