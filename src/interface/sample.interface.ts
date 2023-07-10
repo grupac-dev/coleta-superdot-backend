d8672753255f32bc747da976736aa949b37ef31c
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-10 16:13:53 -0300

    new: SampleReview interface and schema

--- src/interface/sample.interface.ts
+++ src/interface/sample.interface.ts
@@ -1,25 +1,28 @@
+import { InstituitionType, SampleStatus } from "../util/consts";
+import ISampleReview from "./sampleReview.interface";
+
 export default interface ISample {
     _id?: string;
     research_title: string;
     sample_title: string;
     sample_group: string;
     qtt_participants_requested: number;
-    qtt_participantes_authorized?: number;
+    qtt_participants_authorized?: number;
     research_cep: {
         cep_code: string;
         research_document?: string;
         tcle_document?: string;
         tale_document?: string;
     };
-    status?: "Autorizado" | "Pendente" | "Não Autorizado";
+    status?: SampleStatus;
     country_region: string;
     country_state: string;
     country_city: string;
     instituition: {
         name: string;
-        type: "Pública" | "Particular";
+        instType: InstituitionType;
     };
-    reviews?: [];
+    reviews?: [ISampleReview];
     participants?: [];
     createdAt?: Date;
     updatedAt?: Date;
