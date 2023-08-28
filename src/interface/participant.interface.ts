0ec52b5d9ccb6edac2624079b54f81d9bf4250a2
DVastMasters <contatorafaeldutra@outlook.com>
2023-08-28 11:39:17 -0300

    feat: created an interface to participant objects

---
+++ src/interface/participant.interface.ts
@@ -0,0 +1,30 @@
+import { DeviceType, EducationLevelType, GenderType, IncomeLevelType, MaritalStatusType } from "../util/consts";
+
+export interface IParticipant {
+    _id?: string;
+    personalData: {
+        fullName: string;
+        phone: string;
+        email: string;
+        maritalStatus: MaritalStatusType;
+        job: string;
+        occupation: string;
+        educationLevel: EducationLevelType;
+        gender: GenderType;
+        birthDate: Date;
+    };
+    familyData: {
+        qttChildrens: number;
+        qttSiblings: number;
+        qttFamilyMembers: string;
+        familyMonthIncome: IncomeLevelType;
+        houseDevices?: DeviceType[];
+        outsideHouseDevices?: DeviceType[];
+    };
+    addressData: {
+        city: string;
+        district: string;
+        street: string;
+        houseNumber: string;
+    };
+}
