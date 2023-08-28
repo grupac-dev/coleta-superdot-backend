4f56efaf70fc698fe4a04754660fdd30ecedacd6
DVastMasters <contatorafaeldutra@outlook.com>
2023-08-28 09:42:59 -0300

    feat: added a roles const

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -1,5 +1,11 @@
 export const SAMPLE_STATUS_ARRAY = ["Pendente", "Autorizado", "Não Autorizado"] as const;
-export type SampleStatus = "Pendente" | "Autorizado" | "Não Autorizado";
+export type SampleStatus = (typeof SAMPLE_STATUS_ARRAY)[number];
 
 export const INSTITUITION_TYPE_ARRAY = ["Pública", "Particular"] as const;
-export type InstituitionType = "Pública" | "Particular";
+export type InstituitionType = (typeof INSTITUITION_TYPE_ARRAY)[number];
+
+export const FORM_FILL_STATUS = ["Preenchendo", "Aguardando 2ª fonte", "Finalizado"] as const;
+export type FormFillStatusType = (typeof FORM_FILL_STATUS)[number];
+
+export const ROLES = ["Pesquisador", "Revisor", "Administrador"] as const;
+export type RolesType = (typeof ROLES)[number];
