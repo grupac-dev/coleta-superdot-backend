0b799a630808ea8ba3c761b425661a6b8039f46b
DVastMasters <contatorafaeldutra@outlook.com>
2023-08-29 11:48:39 -0300

    feat: created a route to accept sample docs

--- src/route/adultForm.route.ts
+++ src/route/adultForm.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { validateDTO } from "../middleware/validateDTO.middleware";
-import { adultFormParticipantDataDTO } from "../dto/adultForm.dto";
+import { adultFormAcceptDocsDTO, adultFormParticipantDataDTO } from "../dto/adultForm.dto";
 import * as AdultFormController from "../controller/adultForm.controller";
+import { requireParticipantJWT } from "../middleware/requireParticipantJWT";
 
 const adultFormRoute = express.Router();
 
@@ -9,6 +10,13 @@ adultFormRoute.post(
     "/startFillForm/:sampleId",
     validateDTO(adultFormParticipantDataDTO),
     AdultFormController.handlerStartFillForm
+);
+
+adultFormRoute.patch(
+    "/acceptDocs/sample/:sampleId/participant/:participantId",
+    validateDTO(adultFormAcceptDocsDTO),
+    requireParticipantJWT,
+    AdultFormController.handlerAcceptDocs
 );
 
 export { adultFormRoute };
