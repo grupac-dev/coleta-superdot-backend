7dcb04236cf3fa82b3f03bddcb0c01f2e8e9228a
DVastMasters <contatorafaeldutra@outlook.com>
2023-09-08 11:02:41 -0300

    refactor: unused adult form service functions

--- src/service/adultForm.service.ts
+++ src/service/adultForm.service.ts
@@ -1,50 +1,13 @@
 import ResearcherModel from "../model/researcher.model";
-import { IParticipant } from "../interface/participant.interface";
 import { signJwt } from "../util/jwt";
 import env from "../util/validateEnv";
 import mongoose from "mongoose";
-import { ISecondSource } from "../interface/secondSource.interface";
-import * as EmailUtils from "../util/emailSender.util";
+import { EAdultFormGroup, EAdultFormSource, EAdultFormSteps } from "../util/consts";
+import SourceQuestionsGroupModel from "../model/adultForm/sourceQuestionsGroup.model";
+import IQuestionsGroup from "../interface/adultForm/questionsGroup.interface";
 
-export async function createParticipant(sampleId: string, participantData: IParticipant): Promise<string> {
-    if (!mongoose.Types.ObjectId.isValid(sampleId)) {
-        throw new Error("Sample id is invalid.");
-    }
-
-    const researcherDoc = await ResearcherModel.findOne({ "researchSamples._id": sampleId });
-
-    if (!researcherDoc || !researcherDoc.researchSamples) {
-        throw new Error("Sample not found.");
-    }
-
-    const sample = researcherDoc.researchSamples.find((sample) => sample._id?.toString() === sampleId);
-
-    if (!sample) {
-        throw new Error("Sample not found.");
-    }
-
-    const participantWithSameEmail = sample.participants?.find(
-        (participant) => participant.personalData.email === participantData.personalData.email
-    );
-
-    if (participantWithSameEmail) {
-        throw new Error("Email already registered.");
-    }
-
-    sample.participants?.push(participantData);
-
-    await researcherDoc.save();
-
-    const participantCreated = sample.participants?.find(
-        (participant) => (participant.personalData.email = participantData.personalData.email)
-    );
-
-    if (!participantCreated || !participantCreated._id) {
-        throw new Error("The participant participant object was not created.");
-    }
-
-    return issueParticipantToken(participantCreated?._id);
-}
+// The quantity of form steps that exists before the participant start answer the form groups questions.
+const QTT_STEP_BEFORE_GROUPS_QUESTIONS = 4;
 
 export function issueParticipantToken(participantId: string) {
     const accessToken = signJwt(
@@ -60,45 +23,37 @@ export function issueParticipantToken(participantId: string) {
     return accessToken;
 }
 
-export async function acceptDocs(sampleId: string, participantId: string) {
-    if (!mongoose.Types.ObjectId.isValid(sampleId)) {
-        throw new Error("Sample id is invalid.");
+export async function getQuestionsByGroup(source: EAdultFormSource, groupSequence: EAdultFormGroup) {
+    const sourceQuestionsGroup = await SourceQuestionsGroupModel.findOne({
+        source,
+        "groups.sequence": groupSequence,
+    });
+
+    if (!sourceQuestionsGroup || !sourceQuestionsGroup.groups) {
+        throw Error("Group questions not found");
     }
 
-    const researcherDoc = await ResearcherModel.findOne({ "researchSamples._id": sampleId });
+    const group = sourceQuestionsGroup.groups.find((group) => Number(group.sequence) === Number(groupSequence));
 
-    if (!researcherDoc || !researcherDoc.researchSamples) {
-        throw new Error("Sample not found.");
+    if (!group) {
+        throw Error("Group questions not found");
     }
 
-    const sample = researcherDoc.researchSamples.find((sample) => sample._id?.toString() === sampleId);
-
-    if (!sample) {
-        throw new Error("Sample not found.");
-    }
-
-    const participant = sample.participants?.find((participant) => participant._id?.toString() === participantId);
-
-    if (!participant) {
-        throw new Error("Participant not found.");
-    }
-
-    if (sample.researchCep.taleDocument) {
-        participant.acceptTale = true;
-    }
-
-    if (sample.researchCep.tcleDocument) {
-        participant.acceptTcle = true;
-    }
-
-    await researcherDoc.save();
-
-    return true;
+    return group;
 }
 
-export async function createSecondSource(sampleId: string, participantId: string, secondSources: ISecondSource[]) {
+export async function submitGroupQuestions(
+    sampleId: string,
+    participantId: string,
+    groupQuestionsAndAnswers: IQuestionsGroup,
+    secondSourceId?: string
+) {
     if (!mongoose.Types.ObjectId.isValid(sampleId)) {
         throw new Error("Sample id is invalid.");
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(participantId)) {
+        throw new Error("Participant id is invalid.");
     }
 
     const researcherDoc = await ResearcherModel.findOne({ "researchSamples._id": sampleId });
@@ -119,29 +74,64 @@ export async function createSecondSource(sampleId: string, participantId: string
         throw new Error("Participant not found.");
     }
 
-    secondSources.forEach((secondSource) => {
-        const secondSourceWithSameEmail = participant.secondSources?.find(
-            (secondSourceRegistered) => secondSourceRegistered.personalData.email === secondSource.personalData.email
+    // The participant is a second source
+    if (secondSourceId) {
+        const secondSource = participant.secondSources?.find(
+            (secondSource) => secondSource._id?.toString() === secondSourceId
         );
 
-        if (secondSourceWithSameEmail) {
-            throw new Error("Email already registered to other second source.");
+        if (!secondSource) {
+            throw new Error("Second source not found.");
         }
 
-        participant?.secondSources?.push(secondSource);
-    });
+        if (secondSource.adultFormAnswers) {
+            secondSource.adultFormAnswers?.push(groupQuestionsAndAnswers);
+        } else {
+            secondSource.adultFormAnswers = [groupQuestionsAndAnswers];
+        }
+
+        // Next FORM step.
+        // Form steps can be: 0 - 10 (see EAdultFormGroup)
+        // Group questions can be: 0 - 5 (see EAdultFormSteps)
+        secondSource.adultFormCurrentStep = QTT_STEP_BEFORE_GROUPS_QUESTIONS + groupQuestionsAndAnswers.sequence + 1;
+
+        let returnValue: IQuestionsGroup | boolean = true;
+
+        // Last group
+        if (groupQuestionsAndAnswers.sequence === EAdultFormGroup.ARTISTIC_ACTIVITIES) {
+            // Jump autobiography
+            secondSource.adultFormCurrentStep = EAdultFormSteps.FINISHED;
+            secondSource.endFillFormDate = new Date().toISOString();
+        } else {
+            returnValue = await getQuestionsByGroup(
+                EAdultFormSource.SECOND_SOURCE,
+                groupQuestionsAndAnswers.sequence + 1
+            );
+        }
+
+        await researcherDoc.save();
+
+        return returnValue;
+    }
+
+    // Participant is first source
+    if (participant.adultFormAnswers) {
+        participant.adultFormAnswers?.push(groupQuestionsAndAnswers);
+    } else {
+        participant.adultFormAnswers = [groupQuestionsAndAnswers];
+    }
+
+    // Next FORM step.
+    // Form steps can be: 0 - 10 (see EAdultFormGroup)
+    // Group questions can be: 0 - 5 (see EAdultFormSteps)
+    participant.adultFormCurrentStep = QTT_STEP_BEFORE_GROUPS_QUESTIONS + groupQuestionsAndAnswers.sequence + 1;
 
     await researcherDoc.save();
 
-    /** SEDING EMAIL */
-    secondSources.forEach((secondSource) => {
-        EmailUtils.dispatchSecondSourceIndicationEmail({
-            participantName: participant.personalData.fullName,
-            participantEmail: participant.personalData.email,
-            secondSourceName: secondSource.personalData.fullName,
-            secondSourceEmail: secondSource.personalData.email,
-        });
-    });
+    // Last group
+    if (groupQuestionsAndAnswers.sequence === EAdultFormGroup.ARTISTIC_ACTIVITIES) {
+        return true;
+    }
 
-    return true;
+    return getQuestionsByGroup(EAdultFormSource.FIRST_SOURCE, groupQuestionsAndAnswers.sequence + 1);
 }
