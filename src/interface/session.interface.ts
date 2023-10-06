7cdd018c05db2a1ddac504d42fb71c9d16bb9cfc
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-06 11:47:00 -0300

    feat: removing session objects

--- src/interface/session.interface.ts
+++
@@ -1,9 +0,0 @@
-import Researcher from "./researcher.interface";
-
-export interface ISession {
-    _id?: string;
-    researcherId: Researcher["_id"];
-    valid: boolean;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
