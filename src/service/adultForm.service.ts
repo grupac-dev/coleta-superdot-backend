cf660127d795d62eee5d346e36b063ebc1f51116
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-11 10:59:24 -0300

    feat: participant can save form without submit

--- src/service/adultForm.service.ts
+++ src/service/adultForm.service.ts
@@ -1,39 +1,17 @@
-import ResearcherModel from "../model/researcher.model";
-import { signJwt } from "../util/jwt";
-import env from "../util/validateEnv";
-import mongoose from "mongoose";
 import { EAdultFormGroup, EAdultFormSource, EAdultFormSteps, EQuestionType } from "../util/consts";
 import SourceQuestionsGroupModel from "../model/adultForm/sourceQuestionsGroup.model";
 import IQuestionsGroup from "../interface/adultForm/questionsGroup.interface";
 import IQuestion from "../interface/adultForm/question.interface";
+import { ISecondSource } from "../interface/secondSource.interface";
+import { dispatchSecondSourceIndicationEmail } from "../util/emailSender.util";
+import { findParticipantById } from "./participant.service";
+import { getSampleById } from "./sample.service";
 
 // The quantity of form steps that exists before the participant start answer the form groups questions.
 const QTT_STEP_BEFORE_GROUPS_QUESTIONS = 4;
 
-export function issueParticipantToken(participantId: string) {
-    const accessToken = signJwt(
-        {
-            participantId,
-        },
-        "ACCESS_TOKEN_PRIVATE_KEY",
-        {
-            expiresIn: env.ACCESS_TOKEN_TTL,
-        }
-    );
-
-    return accessToken;
-}
-
-interface IQuestionWithoutPoints extends Omit<IQuestion, "options"> {
-    options: string[];
-}
-
-interface IGroupQuestionsWithoutPoints extends Omit<IQuestionsGroup, "questions"> {
-    questions: IQuestionWithoutPoints;
-}
-
 export async function getQuestionsByGroup(sourceForm: EAdultFormSource, groupSequence: EAdultFormGroup) {
-    const sourceQuestionsGroup = await SourceQuestionsGroupModel.aggregate<IGroupQuestionsWithoutPoints>()
+    const sourceQuestionsGroup = await SourceQuestionsGroupModel.aggregate<IQuestionsGroup>()
         .match({ source: Number(sourceForm) })
         .unwind("$groups")
         .match({ "groups.sequence": Number(groupSequence) })
@@ -41,24 +19,7 @@ export async function getQuestionsByGroup(sourceForm: EAdultFormSource, groupSeq
             _id: false,
             groupName: "$groups.groupName",
             sequence: "$groups.sequence",
-            questions: {
-                $map: {
-                    input: "$groups.questions",
-                    in: {
-                        _id: "$$this._id",
-                        sequence: "$$this.sequence",
-                        questionType: "$$this.questionType",
-                        statement: "$$this.statement",
-                        options: {
-                            $map: {
-                                input: "$$this.options",
-                                in: "$$this.value",
-                            },
-                        },
-                        notRequired: "$$this.notRequired",
-                    },
-                },
-            },
+            questions: "$groups.questions",
         });
 
     if (!sourceQuestionsGroup || sourceQuestionsGroup.length !== 1) {
@@ -68,90 +29,71 @@ export async function getQuestionsByGroup(sourceForm: EAdultFormSource, groupSeq
     return sourceQuestionsGroup[0];
 }
 
-export async function submitGroupQuestions(
+export async function saveGroupQuestions(
     sampleId: string,
     participantId: string,
     groupQuestionsAndAnswers: IQuestionsGroup,
     secondSourceId?: string
 ) {
-    if (!mongoose.Types.ObjectId.isValid(sampleId)) {
-        throw new Error("Sample id is invalid.");
-    }
+    const { researcherDoc, sample } = await getSampleById({ sampleId });
+    const participant = findParticipantById({ sample, participantId });
 
-    if (!mongoose.Types.ObjectId.isValid(participantId)) {
-        throw new Error("Participant id is invalid.");
-    }
+    // // The participant is a second source
+    // if (secondSourceId) {
+    //     const secondSource = participant.secondSources?.find(
+    //         (secondSource) => secondSource._id?.toString() === secondSourceId
+    //     );
 
-    const researcherDoc = await ResearcherModel.findOne({ "researchSamples._id": sampleId });
+    //     if (!secondSource) {
+    //         throw new Error("Second source not found.");
+    //     }
 
-    if (!researcherDoc || !researcherDoc.researchSamples) {
-        throw new Error("Sample not found.");
-    }
+    //     const groupWithPontuation = await calculatePontuation(groupQuestionsAndAnswers, EAdultFormSource.SECOND_SOURCE);
 
-    const sample = researcherDoc.researchSamples.find((sample) => sample._id?.toString() === sampleId);
+    //     if (secondSource.adultFormAnswers) {
+    //         secondSource.adultFormAnswers?.push(groupWithPontuation);
+    //     } else {
+    //         secondSource.adultFormAnswers = [groupWithPontuation];
+    //     }
 
-    if (!sample) {
-        throw new Error("Sample not found.");
-    }
+    //     // Next FORM step.
+    //     // Form steps can be: 0 - 10 (see EAdultFormGroup)
+    //     // Group questions can be: 0 - 5 (see EAdultFormSteps)
+    //     secondSource.adultFormCurrentStep = QTT_STEP_BEFORE_GROUPS_QUESTIONS + groupWithPontuation.sequence + 1;
 
-    const participant = sample.participants?.find((participant) => participant._id?.toString() === participantId);
+    //     let returnValue: IGroupQuestionsWithoutPoints | boolean = true;
 
-    if (!participant) {
-        throw new Error("Participant not found.");
-    }
+    //     // Last group
+    //     if (groupWithPontuation.sequence === EAdultFormGroup.ARTISTIC_ACTIVITIES) {
+    //         // Jump autobiography
+    //         secondSource.adultFormCurrentStep = EAdultFormSteps.FINISHED;
+    //         secondSource.endFillFormDate = new Date().toISOString();
+    //     } else {
+    //         returnValue = await getQuestionsByGroup(EAdultFormSource.SECOND_SOURCE, groupWithPontuation.sequence + 1);
+    //     }
 
-    // The participant is a second source
-    if (secondSourceId) {
-        const secondSource = participant.secondSources?.find(
-            (secondSource) => secondSource._id?.toString() === secondSourceId
+    //     await researcherDoc.save();
+
+    //     return returnValue;
+    // }
+
+    const groupWithPontuation = await calculatePunctuation(groupQuestionsAndAnswers, EAdultFormSource.FIRST_SOURCE);
+
+    if (participant.adultForm?.answersByGroup) {
+        const idxFromGroupAlreadyAdded = participant.adultForm?.answersByGroup?.findIndex(
+            (group) => group.sequence === groupWithPontuation.sequence
         );
 
-        if (!secondSource) {
-            throw new Error("Second source not found.");
-        }
+        console.log(idxFromGroupAlreadyAdded);
 
-        const groupWithPontuation = await calculatePontuation(groupQuestionsAndAnswers, EAdultFormSource.SECOND_SOURCE);
-
-        if (secondSource.adultFormAnswers) {
-            secondSource.adultFormAnswers?.push(groupWithPontuation);
+        if (idxFromGroupAlreadyAdded > -1) {
+            participant.adultForm.answersByGroup[idxFromGroupAlreadyAdded] = groupWithPontuation;
         } else {
-            secondSource.adultFormAnswers = [groupWithPontuation];
+            participant.adultForm.answersByGroup.push(groupWithPontuation);
         }
-
-        // Next FORM step.
-        // Form steps can be: 0 - 10 (see EAdultFormGroup)
-        // Group questions can be: 0 - 5 (see EAdultFormSteps)
-        secondSource.adultFormCurrentStep = QTT_STEP_BEFORE_GROUPS_QUESTIONS + groupWithPontuation.sequence + 1;
-
-        let returnValue: IGroupQuestionsWithoutPoints | boolean = true;
-
-        // Last group
-        if (groupWithPontuation.sequence === EAdultFormGroup.ARTISTIC_ACTIVITIES) {
-            // Jump autobiography
-            secondSource.adultFormCurrentStep = EAdultFormSteps.FINISHED;
-            secondSource.endFillFormDate = new Date().toISOString();
-        } else {
-            returnValue = await getQuestionsByGroup(EAdultFormSource.SECOND_SOURCE, groupWithPontuation.sequence + 1);
-        }
-
-        await researcherDoc.save();
-
-        return returnValue;
-    }
-
-    const groupWithPontuation = await calculatePontuation(groupQuestionsAndAnswers, EAdultFormSource.FIRST_SOURCE);
-
-    // Participant is first source
-    if (participant.adultFormAnswers) {
-        participant.adultFormAnswers?.push(groupWithPontuation);
     } else {
-        participant.adultFormAnswers = [groupWithPontuation];
+        participant.adultForm?.answersByGroup?.push(groupWithPontuation);
     }
-
-    // Next FORM step.
-    // Form steps can be: 0 - 10 (see EAdultFormGroup)
-    // Group questions can be: 0 - 5 (see EAdultFormSteps)
-    participant.adultFormCurrentStep = QTT_STEP_BEFORE_GROUPS_QUESTIONS + groupWithPontuation.sequence + 1;
 
     await researcherDoc.save();
 
@@ -163,7 +105,19 @@ export async function submitGroupQuestions(
     return getQuestionsByGroup(EAdultFormSource.FIRST_SOURCE, groupWithPontuation.sequence + 1);
 }
 
-export async function calculatePontuation(participantAnswers: IQuestionsGroup, source: EAdultFormSource) {
+/**
+ * The function calculates the punctuation for a participant's answers based on a set of questions and
+ * their corresponding options.
+ * @param {IQuestionsGroup} participantAnswers - The `participantAnswers` parameter is an object of
+ * type `IQuestionsGroup`, which represents the answers provided by a participant for a group of
+ * questions.
+ * @param {EAdultFormSource} source - The `source` parameter is of type `EAdultFormSource`, which is an
+ * enum representing the source of the adult form. It could have values like
+ * `EAdultFormSource.FIRST_SOURCE` or `EAdultFormSource.SECOND_SOURCE`.
+ * @returns an object that contains the participant's answers with punctuation calculated for each
+ * question.
+ */
+export async function calculatePunctuation(participantAnswers: IQuestionsGroup, source: EAdultFormSource) {
     const sourceGroup = await SourceQuestionsGroupModel.findOne({ source });
 
     const group = sourceGroup?.groups.find((group) => group.sequence === participantAnswers.sequence);
@@ -173,23 +127,15 @@ export async function calculatePontuation(participantAnswers: IQuestionsGroup, s
     }
 
     const questionsWithPontuation = participantAnswers.questions.map((question) => {
-        const questionInDB = group.questions.find((questionDB) => questionDB.sequence === question.sequence);
+        const questionInDB = group.questions.find((questionDB) => questionDB._id === question._id);
 
         if (!questionInDB) return question;
 
         if (!questionInDB.options) return question;
 
-        // The answer doesn't impact in the pontuation, only is necessary to have a answer
-        if (questionInDB.constantPunctuation && question.answer) {
-            if (question.answer)
-                return {
-                    ...question,
-                    answerPoints: questionInDB.constantPunctuation,
-                };
-        }
-
         return {
             ...question,
+            options: questionInDB.options, // Avoid changing punctuation in frontend
             answerPoints: questionInDB.options.find((option) => option.value === question.answer)?.points,
         };
     });
@@ -198,4 +144,34 @@ export async function calculatePontuation(participantAnswers: IQuestionsGroup, s
         ...participantAnswers,
         questions: questionsWithPontuation,
     };
+}
+
+interface FinishFormParams {
+    participantId: string;
+    sampleId: string;
+    secondSourcesToDispatchEmails: ISecondSource[];
+}
+
+export async function finishForm({ participantId, sampleId, secondSourcesToDispatchEmails }: FinishFormParams) {
+    const { researcherDoc, sample } = await getSampleById({ sampleId });
+    const participant = findParticipantById({ sample, participantId });
+
+    if (participant.adultForm) {
+        participant.adultForm.endFillFormAt = new Date();
+    }
+
+    participant.verification = undefined;
+
+    await researcherDoc.save();
+
+    secondSourcesToDispatchEmails.forEach((secondSource) => {
+        dispatchSecondSourceIndicationEmail({
+            participantName: participant.personalData?.fullName as string,
+            participantEmail: participant.personalData?.email as string,
+            secondSourceName: secondSource.personalData.fullName,
+            secondSourceEmail: secondSource.personalData.email,
+            participantId: participantId,
+            sampleId: sampleId,
+        });
+    });
 }
