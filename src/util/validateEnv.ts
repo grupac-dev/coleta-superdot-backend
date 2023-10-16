054b0e3c253fa5bdb5e8e94d94cc7e4d109953af
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-16 08:50:17 -0300

    fix: setup a env variable to store the salt rounds qtt

--- src/util/validateEnv.ts
+++ src/util/validateEnv.ts
@@ -2,7 +2,7 @@ import { cleanEnv } from "envalid";
 import { num, str } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
-    SALT_STRING: str(),
+    SALT_ROUNDS: num(),
     ACCESS_TOKEN_PRIVATE_KEY: str(),
     ACCESS_TOKEN_PUBLIC_KEY: str(),
     REFRESH_TOKEN_PRIVATE_KEY: str(),
